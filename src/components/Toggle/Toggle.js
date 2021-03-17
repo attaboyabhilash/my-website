bcdf5e5c18bdf1c21d9d06b0469d9fea6aa7e34d
Abhilash Negi <tiger.abhi.3609@gmail.com>
2021-03-17 21:01:20 +0530

    new domain added

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { FiSun, FiMoon } from "react-icons/fi"
+import { Switch } from "antd"
 import { ThemeContext } from "../../context/ThemeContext"
 import styles from "./Toggle.module.scss"
 
@@ -8,23 +9,12 @@ const Toggle = () => {
 
     return (
         <div className={styles.toggles}>
-            <label>
-                <input
-                    className={styles.toggle_checkbox}
-                    type="checkbox"
-                    checked={darkMode}
-                    onChange={toggleMode}
-                ></input>
-                <div className={styles.toggle_slot}>
-                    <div className={styles.sun_icon_wrapper}>
-                        <FiSun className={styles.sun_icon} />
-                    </div>
-                    <div className={styles.toggle_button}></div>
-                    <div className={styles.moon_icon_wrapper}>
-                        <FiMoon className={styles.moon_icon} />
-                    </div>
-                </div>
-            </label>
+            <Switch
+                checkedChildren={<FiMoon />}
+                unCheckedChildren={<FiSun />}
+                onChange={toggleMode}
+                checked={darkMode}
+            />
         </div>
     )
 }
