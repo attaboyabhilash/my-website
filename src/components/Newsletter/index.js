f4f14b72e139b7e79ba59184faae21c75579bc35
Abhilash Negi <tiger.abhi.3609@gmail.com>
2021-03-18 18:30:34 +0530

    date updated to current date

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -1,37 +1,70 @@
-import { useContext } from "react"
+import { useState, useContext, useRef } from "react"
 import { Form, Input, Button } from "antd"
 import styles from "./Newsletter.module.scss"
 import { ThemeContext } from "../../context/ThemeContext"
 
 const index = () => {
+    const [isLoading, setIsLoading] = useState(false)
     const { darkMode } = useContext(ThemeContext)
+    const formRef = useRef()
+    const [form] = Form.useForm()
+
+    const onFinish = values => {
+        setIsLoading(true)
+        console.log(values)
+        setIsLoading(false)
+    }
     return (
         <div className={styles.main_block}>
-            <h3 className={styles.title}>
-                Get updates everytime a blog is posted
-            </h3>
-            <Form className={styles.sub_form}>
-                <Input
-                    style={
-                        darkMode === "dark"
-                            ? {
-                                  backgroundColor: "#333",
-                                  color: "#f4f4f4",
-                                  borderColor: "#333",
-                              }
-                            : {
-                                  backgroundColor: "#FFF",
-                                  color: "#000",
-                                  borderColor: "#d9d9d9",
-                              }
-                    }
-                    placeholder="someone@example.com"
-                />
-                <Button type="primary">Subscribe</Button>
-            </Form>
-            <small className={styles.sub_title}>
-                You can unsubscribe at any time.
+            <h3 className={styles.title}>Join The Newsletter</h3>
+            <small className={styles.sub_title} style={{ lineHeight: "0.9em" }}>
+                Get updates everytime a blog is posted. You can unsubscribe at
+                any time.
             </small>
+            <Form
+                className={styles.sub_form}
+                layout="vertical"
+                form={form}
+                ref={formRef}
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            type: "email",
+                            message: "Email Address is invalid",
+                        },
+                    ]}
+                >
+                    <Input
+                        style={
+                            darkMode === "dark"
+                                ? {
+                                      backgroundColor: "#333",
+                                      color: "#f4f4f4",
+                                      borderColor: "#333",
+                                  }
+                                : {
+                                      backgroundColor: "#FFF",
+                                      color: "#000",
+                                      borderColor: "#d9d9d9",
+                                  }
+                        }
+                        placeholder="someone@example.com"
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        loading={isLoading}
+                    >
+                        Subscribe
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     )
 }
