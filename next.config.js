83c0c8c736de07e32e0a24ea18bb88ca99d38181
Abhilash Negi <tiger.abhi.3609@gmail.com>
2021-03-18 15:40:03 +0530

    blog is redirected to blogs

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ const withMDX = require("@next/mdx")()
 
 module.exports = withMDX({
     pageExtensions: ["js", "mdx"],
+    async redirects() {
+        return [
+            {
+                source: "/blog",
+                destination: "/blogs",
+                permanent: true,
+            },
+        ]
+    },
 })
